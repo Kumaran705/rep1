ba9ef36dc83d86eb8b5cd4e21860255e2f51454e
Kumaran705 <kk6389280@gmail.com>
2025-11-03 11:03:30 +0530

    An line is added

--- Hollow.java
+++ Hollow.java
@@ -2,5 +2,6 @@ import java.util.*;
 public class Hollow{
   public static void main(String args[]){
     System.out.println("This the Hollow File");
+    System.out.println("An extra line is added");
   }
 }
